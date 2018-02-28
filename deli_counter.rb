ab20b061680292e530c9e5082ba483cafa32b762
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 20:37:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,4 @@
 # Write your code here.
-kats_deli = []
 
 def line(arr)
   if arr.count > 0
@@ -13,4 +12,7 @@ def line(arr)
   else
     puts "The line is currently empty."
   end
+end
+
+def take_a_number(arr)
 end
